b15e885275dd9eda6ea4fd487e55292f668fde82
Neel.Chhantbar <neel.chhantbar@tssconsultancy.com>
2026-04-28 14:36:04 +0530

    feat: saving jwt in cookies

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,17 +1,32 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { environment } from '../../../environments/environment';
+import { StorageService } from './storage.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private apiUrl = environment.apiUrl;
-
-  constructor(private http: HttpClient) {}
+  private http = inject(HttpClient);
+  private storageService = inject(StorageService);
 
   login(payload: any): Observable<any> {
-    return this.http.post(`${this.apiUrl}/login`, payload);
+    return this.http.post<any>(`${this.apiUrl}/login`, payload).pipe(
+      tap(response => {
+        if (response && response.jwt) {
+          this.storageService.setCookie('auth_token', response.jwt);
+        }
+      })
+    );
+  }
+
+  getToken(): string | null {
+    return this.storageService.getCookie('auth_token');
+  }
+
+  logout() {
+    this.storageService.deleteCookie('auth_token');
   }
 }
